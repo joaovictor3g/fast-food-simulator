4b6cb8ca8319e42a32f73aa389830cb8c7d72e97
emilianots <emiliano@alu.ufc.br>
2021-07-10 19:24:14 -0300

    remake back end project

---
+++ src/test/java/com/ufc/fastfoodsimulator/FastFoodSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.fastfoodsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
