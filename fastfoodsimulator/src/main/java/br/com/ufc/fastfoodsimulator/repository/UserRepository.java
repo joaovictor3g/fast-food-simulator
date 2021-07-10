b7fb7e9e98cade31d5d515d8b2d40451cd2859fb
João Victor <jvdias79797@gmail.com>
2021-07-10 15:16:13 -0300

    first commit

---
+++ fastfoodsimulator/src/main/java/br/com/ufc/fastfoodsimulator/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package br.com.ufc.fastfoodsimulator.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface UserRepository {}
